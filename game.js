d72c9845b09d47b2ca4e7221970c0b036bc41275
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2022-02-07 20:27:51 +0300

    Update game.js

--- game.js
+++ game.js
@@ -2,4 +2,5 @@ function BreakBlock(el) {
   el.style["pointer-events"] = "none";
   el.style.opacity = "0";
 }
+var brokenBlocks = 0;
 document.querySelectorAll(".breakable").forEach((el) => {el.addEventListener("mousedown", function(e) {BreakBlock(el); var aud = new Audio("sbbr.mp3"); aud.play();})});
