7f0af075534f0b91cc32df9d4b1f25e46725e617
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2022-02-07 21:15:42 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
+var brokenBlocks = 0;
 function BreakBlock(el) {
   el.style["pointer-events"] = "none";
   el.style.opacity = "0";
+  brokenBlocks++;
 }
-var brokenBlocks = 0;
 document.querySelectorAll(".breakable").forEach((el) => {if (brokenBlocks < 5) {el.addEventListener("mousedown", function(e) {BreakBlock(el); var aud = new Audio("sbbr.mp3"); aud.play();})} else {document.getElementById("lvl1").style.display = "none"; document.getElementById("lvl2").style.display = null;} if (brokenBlocks < 15) {el.addEventListener("mousedown", function(e) {BreakBlock(el); var aud = new Audio("sbbr.mp3"); aud.play();})} else {alert("Completed!");}});
