305a7148a3d3958e1d5619cd4d0cf503f5b3420c
huseyin31625 <77965087+huseyin31625@users.noreply.github.com>
2022-02-08 21:13:19 +0300

    Update game.js

--- game.js
+++ game.js
@@ -18,6 +18,21 @@ function Completed() {
   element.appendChild(element2);
   document.body.appendChild(element);
 }
+function Exception(txt) {
+  var element = document.createElement("div");
+  element.className = "modal-container";
+  var element2 = document.createElement("div");
+  element2.className = "modal-body";
+  var info = document.createElement("div");
+  info.innerHTML = "<h2>Exception occurred!</h2>" + txt;
+  var ok = document.createElement("button");
+  ok.innerHTML = "OK";
+  ok.onclick = function() {element.remove();};
+  element2.appendChild(info);
+  element2.appendChild(ok);
+  element.appendChild(element2);
+  document.body.appendChild(element);
+}
 function BreakBlockLv1(el) {
   el.style["pointer-events"] = "none";
   el.style.opacity = "0";
@@ -40,4 +55,4 @@ function display (seconds) {
   return [minutes, seconds % 60].map(format).join(':')
 }
 var timer = setInterval(function() {seconds++; document.getElementById("timer").innerHTML = display(seconds);}, 1000);
-document.querySelectorAll(".breakable").forEach((el) => {el.addEventListener("mousedown", function(e) {if (lvl == 1) {if (brokenBlockslvl1 != 5) {BreakBlockLv1(el); var aud = new Audio("sbbr.mp3"); aud.play();} if (brokenBlockslvl1 == 5) {lvl++; document.getElementById("lvl1").style.display = "none"; document.getElementById("lvl2").style.display = null;} return;} if (lvl == 2) {if (brokenBlockslvl2 != 10) {BreakBlockLv2(el); var aud = new Audio("sbbr.mp3"); aud.play();} if (brokenBlockslvl2 == 10) {lvl++; document.getElementById("lvl2").style.display = "none"; document.getElementById("lvl3").style.display = null;} return;} if (lvl == 3) {if (brokenBlockslvl3 != 15) {BreakBlockLv3(el); var aud = new Audio("sbbr.mp3"); aud.play();} if (brokenBlockslvl3 == 15) {Completed(); clearInterval(timer);}}});});
+document.querySelectorAll(".breakable").forEach((el) => {el.addEventListener("mousedown", function(e) {if (lvl == 1) {if (brokenBlockslvl1 != 5) {BreakBlockLv1(el); var aud = new Audio("sbbr.mp3"); aud.play();} if (brokenBlockslvl1 == 5) {try {lvl++; document.getElementById("lvl1").style.display = "none"; document.getElementById("lvl2").style.display = null;} catch(e) {Exception(e);}} return;} if (lvl == 2) {if (brokenBlockslvl2 != 10) {BreakBlockLv2(el); var aud = new Audio("sbbr.mp3"); aud.play();} if (brokenBlockslvl2 == 10) {try {lvl++; document.getElementById("lvl2").style.display = "none"; document.getElementById("lvl3").style.display = null;} catch(e) {Exception(e);}} return;} if (lvl == 3) {if (brokenBlockslvl3 != 15) {BreakBlockLv3(el); var aud = new Audio("sbbr.mp3"); aud.play();} if (brokenBlockslvl3 == 15) {Completed(); clearInterval(timer);}}});});
